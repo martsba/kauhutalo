44e21ff1c309c56ec5d81931bdaa919d8e42402d
martsba <62333762+martsba@users.noreply.github.com>
2021-11-08 14:09:14 +0200

    08_11_21_koulussa
    mainmenu

--- Assets/scripts/PlayButton.cs
+++ Assets/scripts/PlayButton.cs
@@ -9,15 +9,4 @@ public class PlayButton : MonoBehaviour
     {
         SceneManager.LoadScene("kauhutalo", LoadSceneMode.Single);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
